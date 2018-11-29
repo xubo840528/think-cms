aea764175b1ba62d3b7126c755534283d4c06dd9
xubo <297459070@qq.com>
2018-11-29 09:29:17 +0800

    dev

---
+++ config/config.php
@@ -0,0 +1,75 @@
+<?php
+return [
+    'order_status'=>[
+        '-4'=> '未取消',
+        '0' => '待咨询',
+        '1' => '咨询中',
+        '2' => '待付款',
+        '3' => '等待客服介入',
+        '4' => '已取消',
+        '5' => '咨询结束'
+    ],
+    'schedule_status'=>[
+        '0' => '未开启',
+        '1' => '已开启',
+        '2' => '已预约',
+        '3' => '已满员',
+    ],
+    'operate_type'=>[
+        '0' => '登出',
+        '1' => '登入',
+        '2' => '创单',
+        '3' => '订单开始',
+        '4'=>'订单结束',
+        '5'=>'订单取消',
+        '6'=>'订单收款',
+        '7'=>'套餐'
+    ],
+    'week'=>[
+        '0'=>'周日',
+        '1'=>'周一',
+        '2'=>'周二',
+        '3'=>'周三',
+        '4'=>'周四',
+        '5'=>'周五',
+        '6'=>'周六',
+    ],
+    'main_url'=>'http://m.mangoxinli.com',
+    'h5'=>[
+        'customers'=>[
+            'schedules'=>'/customers/schedules.html',
+            'historys'=>'/customers/historys.html'
+        ],
+        'counselors'=>[
+            'schedules'=>'/counselors/schedules.html',
+            'historys'=>'/counselors/historys.html'
+        ]
+    ],
+    'apps'=>[
+        'customer'=>['wx3fc708ad63b744c7','deb707a3f31517c8c5bc0effca4bd102'],
+        'counselor'=>['wx3fc708ad63b744c7','deb707a3f31517c8c5bc0effca4bd102'],
+    ],
+    'wx_msg'=>[
+        'customers'=>[
+            'create_order'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'remind_order'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'start'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'end'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'help'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'cancel'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'payment'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'package'=>'NTdEtcOt9kvNvgRT92HyDggRP9g2uM7Vj_1bTKwjRDE'
+        ],
+        'counselors'=>[
+            'create_order'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'remind_order'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'start'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'end'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'help'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'cancel'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'payment'=>'i9CdcAiTIUSbod5HEIbd9uUEmbegQQBxSM3xlZb46KI',
+            'package'=>'NTdEtcOt9kvNvgRT92HyDggRP9g2uM7Vj_1bTKwjRDE'
+        ]
+    ],
+    'sys_mobile'=>'18321553724'
+];
